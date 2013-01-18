fccb64a65eee9e37795c1a26dbd14ffced30519a
Will Conant <will.conant@gmail.com>
2013-01-18 16:16:39 -0700

    My full name is stuffy

--- cot.js
+++ cot.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013 William R. Conant, http://willconant.com/
+Copyright (c) 2013 Will Conant, http://willconant.com/
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
